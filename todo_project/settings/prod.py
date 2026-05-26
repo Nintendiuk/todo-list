4784cf80fee0f7ebbc0ae04d1fb9a099854f883f
Nintendiuk <oleksii.rachynskyi@gmail.com>
2026-05-26 14:39:44 +0300

    chore: initialize project structure with settings, linting, and docs

---
+++ todo_project/settings/prod.py
@@ -0,0 +1,27 @@
+"""
+Production settings.
+Run: export DJANGO_SETTINGS_MODULE=todo_project.settings.prod
+"""
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "").split(",")
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
+        "CONN_MAX_AGE": 60,
+    }
+}
+
+SECURE_BROWSER_XSS_FILTER = True
+SECURE_CONTENT_TYPE_NOSNIFF = True
+X_FRAME_OPTIONS = "DENY"
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
